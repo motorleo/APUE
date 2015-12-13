9cbe50f94c324dff491811951584b5b1a78f0bf6
motorleo <lee3239@outlook.com>
2015-12-13 19:49:33 +0800

    seperate header

--- toys/calc_oop.cc
+++ toys/calc_oop.cc
@@ -1,249 +1,134 @@
-#include <boost/shared_ptr.hpp>
-#include <vector>
-#include <string>
 #include <iostream>
 #include <stdlib.h>
+#include "calc_oop.h"
 
-
-class Node
+int ExpTree::Precedence(const char op)
 {
-public:
-	virtual ~Node() {}
-	
-	virtual int calculate() const =0;
-
-};
-
-typedef boost::shared_ptr<Node> NodePtr;
-
-class NumNode : public Node
-{
-public:
-	NumNode(int num)
-		:val(num){}
-
-	int calculate() const { return val;}
-	
-private:
-	int val;
-};
-
-class BinaryNode : public Node
-{
-public:
-	BinaryNode(const NodePtr& l,const NodePtr& r)
-		:left(l),right(r){}
-
-
-protected:
-	NodePtr left;
-	NodePtr right;
-};
-
-class AddNode : public BinaryNode
-{
-public:
-	AddNode(const NodePtr& l,const NodePtr& r)
-		:BinaryNode(l,r) {}
-	int calculate() const { return left->calculate()+right->calculate();}
-};
-
-class MinusNode : public BinaryNode
-{
-public:
-	MinusNode(const NodePtr& l,const NodePtr& r)
-		:BinaryNode(l,r) {}
-	int calculate() const { return left->calculate()-right->calculate();}
-};
-
-class MultiplyNode : public BinaryNode
-{
-public:
-	MultiplyNode(const NodePtr& l,const NodePtr& r)
-		:BinaryNode(l,r) {}
-	int calculate() const { return left->calculate()*right->calculate();}
-};
-
-class DevideNode : public BinaryNode
-{
-public:
-	DevideNode(const NodePtr& l,const NodePtr& r)
-		:BinaryNode(l,r) {}
-	int calculate() const { return left->calculate()/right->calculate();}
-};
-
-template <typename T>
-class Stack
-{
-public:
-	void push(const T& t) { stack.push_back(t);}
-
-	T pop()
+	switch (op)
 	{
-		T temp=stack.back();
-		stack.pop_back();
-		return temp;
-	}
-
-	const T& peek() { return stack.back();}
-
-	int size() { return stack.size();}
-
-private:
-	std::vector<T> stack;
-};
-
-class ExpTree
-{
-public:
-	ExpTree(const std::string& str) { makeTree(str);}
-
-	int calculate() {return root->calculate();}
-
-private:
-	int Precedence(const char op)
-   	{
-		switch (op)
-	   	{
-			case '+': case '-':
-				return 1;
-			case '*': case '/':
-				return 2;
-			case ')': case '@':
-			default:
+		case '+': case '-':
+			return 1;
+		case '*': case '/':
+			return 2;
+		case ')': case '@':
+		default:
 			//other operator or '@'
-			return 0;  
-		}
+		return 0;  
 	}
+}
 
-	NodePtr makeNode(const char op,const NodePtr& l,const NodePtr& r)
+NodePtr ExpTree::makeNode(const char op,const NodePtr& l,const NodePtr& r)
+{
+	switch (op)
 	{
-		switch (op)
-		{
-			case '+':
-				{
+		case '+':
+			{
 				NodePtr temp(new AddNode(l,r));
 				return temp;
-				}
-			case '-':
-				{
+			}
+		case '-':
+			{
 				NodePtr temp(new MinusNode(l,r));
 				return temp;
-				}
-			case '*':
-				{
+			}
+		case '*':
+			{
 				NodePtr temp(new MultiplyNode(l,r));
 				return temp;
-				}
-			case '/':
-				{
+			}
+		case '/':
+			{
 				NodePtr temp(new DevideNode(l,r));
 				return temp;
-				}
-		}
+			}
 	}
+}
 
-	void makeTree(const std::string& str)
+void ExpTree::makeTree(const std::string& str)
+{
+	Stack<NodePtr> nodeStack;
+	Stack<char> opStack;
+	opStack.push('@');
+	std::string num;
+	for (unsigned int i=0;i < str.size();++i)
 	{
-		Stack<NodePtr> nodeStack;
-		Stack<char> opStack;
-		opStack.push('@');
-		std::string num;
-		for (int i=0;i < str.size();++i)
+		if ('9'>=str[i] && str[i]>='0')
 		{
-			if ('9'>=str[i] && str[i]>='0')
-			{
-				num+=str[i];
-				continue;
-			}
-			if (num.size()>0)
-			{
-				NodePtr temp(new NumNode(atoi(num.c_str())));
-				nodeStack.push(temp);
-				num.clear();
-			}
-			if (str[i] == ' ') { continue; }
-
-			switch (str[i])
-		   	{
-				case '(':
-					opStack.push(str[i]);
-					break;
-				case ')':
-					while (Precedence(opStack.peek())>0) 
-					{
-						NodePtr r=nodeStack.pop();
-						NodePtr	l=nodeStack.pop();
-						nodeStack.push(makeNode(opStack.pop(),l,r));
-					}
-					opStack.pop();
-					break;
-				case '+':case '-':case '*':case '/':
-					while (Precedence(opStack.peek())>=Precedence(str[i]))
-					{
-						NodePtr r=nodeStack.pop();
-						NodePtr	l=nodeStack.pop();
-						nodeStack.push(makeNode(opStack.pop(),l,r));
-					}
-					opStack.push(str[i]);
-					break;
-				default:
-					std::cerr << "illegal operator!\n";
-					exit(1);
-			}
+			num+=str[i];
+			continue;
 		}
-
 		if (num.size()>0)
 		{
 			NodePtr temp(new NumNode(atoi(num.c_str())));
 			nodeStack.push(temp);
+			num.clear();
 		}
+		if (str[i] == ' ') { continue; }
 
-		while (opStack.peek()!='@')
+		switch (str[i])
 		{
-			NodePtr r=nodeStack.pop();
-			NodePtr	l=nodeStack.pop();
-			nodeStack.push(makeNode(opStack.pop(),l,r));
+			case '(':
+				opStack.push(str[i]);
+				break;
+			case ')':
+				while (Precedence(opStack.peek())>0) 
+				{
+					NodePtr r=nodeStack.pop();
+					NodePtr	l=nodeStack.pop();
+					nodeStack.push(makeNode(opStack.pop(),l,r));
+				}
+				opStack.pop();
+				break;
+			case '+':case '-':case '*':case '/':
+				while (Precedence(opStack.peek())>=Precedence(str[i]))
+				{
+					NodePtr r=nodeStack.pop();
+					NodePtr	l=nodeStack.pop();
+					nodeStack.push(makeNode(opStack.pop(),l,r));
+				}
+				opStack.push(str[i]);
+				break;
+			default:
+				std::cerr << "illegal operator!\n";
+				exit(1);
 		}
-
-
-		if (nodeStack.size() != 1)
-		{
-			std::cerr << "illegal expression!\n";
-			exit(2);
-		}
-		root=nodeStack.pop();
-		
 	}
-						
-	NodePtr root;
-};
 
-class Calculator
+	if (num.size()>0)
+	{
+		NodePtr temp(new NumNode(atoi(num.c_str())));
+		nodeStack.push(temp);
+	}
+
+	while (opStack.peek()!='@')
+	{
+		NodePtr r=nodeStack.pop();
+		NodePtr	l=nodeStack.pop();
+		nodeStack.push(makeNode(opStack.pop(),l,r));
+	}
+
+
+	if (nodeStack.size() != 1)
+	{
+		std::cerr << "illegal expression!\n";
+		exit(2);
+	}
+	root=nodeStack.pop();
+
+}
+
+void Calculator::calculate()
 {
-public:
-	void loop()
+	std::string expression;
+	std::cout << "Enter the expression(q for quit): ";
+	std::cin >> expression;
+	if (expression == "q")
 	{
-		while (true)
-		{
-			calculate();
-		}
+		exit(0);
 	}
-
-private:
-	void calculate()
-	{
-		std::string expression;
-		std::cout << "Enter the expression(q for quit): ";
-		std::cin >> expression;
-		if (expression == "q")
-		{
-			exit(0);
-		}
-		ExpTree expTree(expression);
-		std::cout << "The result is : " << expTree.calculate() << std::endl;
-	}	
-};
+	ExpTree expTree(expression);
+	std::cout << "The result is : " << expTree.calculate() << std::endl;
+}	
 
 
 int main()
